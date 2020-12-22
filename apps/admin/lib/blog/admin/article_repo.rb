1707214b45f49aaadb515b13dd9466b8037cdca6
alinschuller <schulleralinmihai@gmail.com>
2020-12-22 10:28:18 +0200

    Add by_id, listing. Create relationship between articles and author

--- apps/admin/lib/blog/admin/article_repo.rb
+++ apps/admin/lib/blog/admin/article_repo.rb
@@ -1,8 +1,27 @@
 require "blog/repository"
+require "blog/admin/entities"
 
 module Blog
   module Admin
     class ArticleRepo < Blog::Repository[:articles]
+      struct_namespace Entities
+
+      commands :create
+
+      def by_id(id)
+        articles.by_pk(id).one
+      end
+
+      def listing
+        articles.order { created_at.desc }.to_a
+      end
+
+      private
+
+      def articles
+        super.combine(:author)
+      end
+
     end
   end
 end
